ba28f128217e435e641a8a4af7eba0cfd4845377
tanvirul15 <tanvirul15-6117@diu.edu.bd>
2021-02-28 12:06:17 +0600

    Partially Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Country from "./components/country/Country";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {countries.map((country) => (
+        <Country country={country} key={country.numericCode} />
+      ))}
     </div>
   );
 }
---
+++ src/components/country/Country.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Country = (props) => {
+  return <div>This is {props.country.name}</div>;
+};
+
+export default Country;
